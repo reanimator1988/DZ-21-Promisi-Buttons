ba9ca9e688f15a47a11dbd89fae27209f1f1e681
DiMa <francev.dima@gmail.com>
2023-07-19 12:00:55 +0200

    dz

--- index.js
+++ index.js
@@ -6,36 +6,55 @@ let oddButtonClicks = 0;
 let evenButtonClicks = 0;
 let uniqueButtonClicks = new Set();
 
-for (let i = 1; i <= buttonCount; i++) {
-    const button = document.createElement('button');
-    button.textContent = i;
-    button.addEventListener('click', function () {
-        if (!button.disabled) {
-            button.style.cssText = "box-shadow: 15px 15px 15px rgb(255, 0, 255); background: #00FFFF; border: none; color: black";
-            uniqueButtonClicks.add(i);
+function handleClick(i) {
+    return new Promise(function(resolve, reject) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.addEventListener('click', function () {
+            if (!button.disabled) {
+                button.style.cssText = "box-shadow: 15px 15px 15px rgb(255, 0, 255); background: #00FFFF; border: none; color: black";
+                uniqueButtonClicks.add(i);
 
-            if (i % 2 !== 0) {
-                oddButtonClicks++;
-                if (oddButtonClicks === Math.ceil(buttonCount / 2)) {
-                    alert('Всі непарні кнопки були натиснуті!');
+                if (i % 2 !== 0) {
+                    oddButtonClicks++;
+                    if (oddButtonClicks === Math.ceil(buttonCount / 2)) {
+                        alert('Всі непарні кнопки були натиснуті!');
+                    }
+                }
+
+                if (i % 2 === 0) {
+                    evenButtonClicks++;
+                    if (evenButtonClicks === Math.floor(buttonCount / 2)) {
+                        alert('Всі парні кнопки були натиснуті!');
+                    }
+                }
+
+                if (uniqueButtonClicks.size === buttonCount) {
+                    setTimeout(function () {
+                        alert("Усі кнопки були натиснуті!");
+                    }, 1000);
+                    resolve();
                 }
             }
-
-            if (i % 2 === 0) {
-                evenButtonClicks++;
-                if (evenButtonClicks === Math.floor(buttonCount / 2)) {
-                    alert('Всі парні кнопки були натиснуті!');
-                }
-            }
-
-            if (uniqueButtonClicks.size === buttonCount) {
-                setTimeout(function () {
-                    alert("Усі кнопки були натиснуті!");
-                }, 1000);
-            }
-        }
-        button.disabled = true;
+            button.disabled = true;
+        });
+        document.body.appendChild(button);
+        button.classList.add('buttons');
     });
-    document.body.appendChild(button);
-    button.classList.add('buttons');
 }
+
+function createButtons() {
+    const promises = [];
+    for (let i = 1; i <= buttonCount; i++) {
+        promises.push(handleClick(i));
+    }
+    return Promise.all(promises);
+}
+
+createButtons()
+    .then(function() {
+        console.log("All buttons created and handled successfully!");
+    })
+    .catch(function(error) {
+        console.error("An error occurred while creating/handling buttons:", error);
+    });
